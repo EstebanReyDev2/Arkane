4cbc58f15e981149a269e646c61ddb3efb9391f5
EstebanReyDev2 <bra.rey.esteban@gmail.com>
2026-04-16 11:28:21 +0000

    Correccion en Page.tsx

--- src/lib/store/cartStore.ts
+++ src/lib/store/cartStore.ts
@@ -2,6 +2,7 @@ import { create } from 'zustand';
 import { persist } from 'zustand/middleware';
 
 export interface CartItem {
+  category: string;
   productId: string;
   variantId: string;
   name: string;
